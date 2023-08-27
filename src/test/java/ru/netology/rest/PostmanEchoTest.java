9464bd183f74d035ac386fdf06115ab206eaf11c
Glazov Nikita <niglazov@gmail.com>
2023-08-27 16:37:33 +0300

    fixed test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("s data"));
+                .body("data", equalTo("some value"));
         ;
     }
 }
